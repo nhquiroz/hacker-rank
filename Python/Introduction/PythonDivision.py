ed42afc7148007291e37cef5bfd05286176f2fff
Nicolás Quiroz <nquiroz@dc.uba.ar>
2015-12-01 12:10:53 -0300

    Add Python Division solution

---
+++ Python/Introduction/PythonDivision.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+
+# Read two integers from STDIN
+a = int(raw_input())
+b = int(raw_input())
+# Print integer division, a//b
+print(a // b)
+# Print float division, a/b
+print(a / b)
