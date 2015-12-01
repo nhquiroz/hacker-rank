957dc18f2e8feb9377c1313d73cd026462ed86f1
Nicolás Quiroz <nquiroz@dc.uba.ar>
2015-12-01 12:28:36 -0300

    Add Mod Divmod solution

---
+++ Python/Introduction/ModDivmod.py
@@ -0,0 +1,12 @@
+from __future__ import division
+
+
+# Read two integers from STDIN
+a = int(raw_input())
+b = int(raw_input())
+# Print integer division, a//b
+print(a // b)
+# Print float division, a/b
+print(a % b)
+# Print divmod of a and b
+print(divmod(a, b))
