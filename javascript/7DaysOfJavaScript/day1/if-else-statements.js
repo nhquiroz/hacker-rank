f5a8438c6762af8a6940486a69e7731d32f2afd6
Nicolas Quiroz <nh.quiroz@gmail.com>
2016-12-20 01:48:20 -0300

    Day 1: If Else Statements

---
+++ javascript/7DaysOfJavaScript/day1/if-else-statements.js
@@ -0,0 +1,17 @@
+if (marks <= 30) {
+	console.log('FF');
+} else if (marks <= 40) {
+	console.log('DD');
+} else if (marks <= 50) {
+	console.log('CD');
+} else if (marks <= 60) {
+	console.log('CC');
+} else if (marks <= 70) {
+	console.log('BC');
+} else if (marks <= 80) {
+	console.log('BB');
+} else if (marks <= 90) {
+	console.log('AB');
+} else {
+	console.log('AA');
+}
--- javascript/7DaysOfJavaScript/day1/print-the-input.js
+++ javascript/7DaysOfJavaScript/day1/print-the-input.js
@@ -1,3 +1,3 @@
 function processData(input) {
-	console.log('Hello World!');
+	console.log(input);
 }
