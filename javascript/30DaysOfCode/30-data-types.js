77e22c0058a73b02bc1ff29ee83f253025f8dd3d
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-12-22 11:19:04 -0300

    Create 30-data-types.js

---
+++ javascript/30DaysOfCode/30-data-types.js
@@ -0,0 +1,19 @@
+// Declare second integer, double, and String variables.
+var secondIntegerVar;
+var doubleVar;
+var stringVar;
+
+// Read and save an integer, double, and String to your variables.
+secondIntegerVar = Number(readLine());
+doubleVar = Number(readLine());
+stringVar = readLine();
+
+// Print the sum of both integer variables on a new line.
+console.log(i + secondIntegerVar);
+
+// Print the sum of the double variables on a new line.
+console.log((d + doubleVar).toFixed(1));
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s + stringVar);
