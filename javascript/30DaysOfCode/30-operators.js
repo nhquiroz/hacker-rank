6f48cae7a1441dcef9add95561ae29990c83f8d5
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-12-23 10:06:18 -0300

    Create 30-operators.js

---
+++ javascript/30DaysOfCode/30-operators.js
@@ -0,0 +1,12 @@
+function main() {
+	var mealCost = Number(readLine());
+	var tipPercentage = Number(readLine());
+	var taxPercentage = Number(readLine());
+
+	var tip = (mealCost * tipPercentage) / 100;
+	var tax = (mealCost * taxPercentage) / 100;
+
+	var totalCost = Math.round(mealCost + tip + tax);
+
+	console.log('The total meal cost is ' + totalCost + ' dollars.');
+}
