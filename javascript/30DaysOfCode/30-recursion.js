ee392d998ff98d9c81a6b3a0fc75a27e8cf9fa87
Nicolás Quiroz <nh.quiroz@gmail.com>
2016-12-30 22:54:13 -0300

    Day 9: Recursion

---
+++ javascript/30DaysOfCode/30-recursion.js
@@ -0,0 +1,13 @@
+function main() {
+	var n = parseInt(readLine());
+
+	console.log(factorial(n));
+
+	function factorial(i) {
+		if (i <= 1) {
+			return 1;
+		}
+
+		return i * factorial(i - 1);
+	}
+}
