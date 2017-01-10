76bd85f8091268460d72392ffead20da057d3bdf
Nicolas Quiroz <nh.quiroz@gmail.com>
2017-01-10 16:59:25 -0300

    Day 15: Linked List

---
+++ javascript/30DaysOfCode/30-linked-list.js
@@ -0,0 +1,13 @@
+function Solution() {
+	this.insert = function (head, data) {
+		if (head === null) {
+			return new Node(data);
+		} else if (head.next === null) {
+			head.next = new Node(data);
+		} else {
+			this.insert(head.next, data);
+		}
+
+		return head;
+	};
+}
